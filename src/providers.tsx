a6ba6556b6de4e9995e2ee61abc1ddef283442e4
lumpen <swk5373@gmail.com>
2024-11-13 02:19:22 +0900

    react query provider 적용

---
+++ src/providers.tsx
@@ -0,0 +1,21 @@
+'use client';
+import React, { PropsWithChildren } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+    },
+  },
+});
+
+export function Providers({ children }: PropsWithChildren) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <main>{children}</main>
+    </QueryClientProvider>
+  );
+}
